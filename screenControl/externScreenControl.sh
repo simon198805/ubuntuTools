7ef6a2045fcf9d1b474412bad274078fabd78c72
taco.chen <taco.chen@besi.com>
2023-06-06 15:07:04 +0800

    fix externScreenControl may go back to normal once changed to idle under some situation

--- screenControl/externScreenControl.sh
+++ screenControl/externScreenControl.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # parameters
+export DISPLAY=:1
+
 EXTSCR="HDMI-1-0"
 EXTSETUP="--auto --above"
 BUILDINSCR="eDP-1"
-# in sec
-IDLE_THRESHOLD=600
-CP_IDLE=1
-CP_WAKE=60
+# in 0.1 sec
+IDLE_THRESHOLD=6000
+# don't set the following value to under 10
+CP_IDLE=10  
+CP_WAKE=300
 
 # used temp variable
 IN_IDLE=false
@@ -15,23 +18,23 @@ IN_IDLE=false
 CHECK_PERIOD=$CP_WAKE
 
 while true; do
-    IDLE_TIME=$(( $(xprintidle) / 1000 ))
+    IDLE_TIME=$(( $(xprintidle) / 100 ))
 
-    if [[ $IDLE_TIME -gt $IDLE_THRESHOLD ]]; then
-        if [ "$IN_IDLE" != true ]; then
+    echo IDLE_TIME $IDLE_TIME
+    if [[ $IDLE_TIME -gt $IDLE_THRESHOLD  && "$IN_IDLE" != true ]]; then
+            # this operation could reset idle time, mouse pos may be changed
             xrandr --output $EXTSCR --off
             IN_IDLE=true
             CHECK_PERIOD=$CP_IDLE
-            echo inIdle
-        fi
-    else
-        if [ "$IN_IDLE" = true ]; then
-            xrandr --output $EXTSCR $EXTSETUP $BUILDINSCR
-            IN_IDLE=false
-            CHECK_PERIOD=$CP_WAKE
-            echo normal
-        fi
+            echo inIdle $IDLE_TIME
     fi
+    
+	if [[ "$IN_IDLE" == true && $IDLE_TIME -lt $CHECK_PERIOD ]]; then
+	    xrandr --output $EXTSCR $EXTSETUP $BUILDINSCR
+	    IN_IDLE=false
+	    CHECK_PERIOD=$CP_WAKE
+	    echo normal $IDLE_TIME
+	fi
 
-    sleep $CHECK_PERIOD
+    sleep $((CHECK_PERIOD / 10))
 done
